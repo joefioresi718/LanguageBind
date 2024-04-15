2375e415d3925759835eed81cbc2ecaaff621125
joefioresi718 <joefioresi718@gmail.com>
2024-04-15 15:32:56 -0400

    move ssv2

--- move_pairs.py
+++ move_pairs.py
@@ -6,21 +6,21 @@ import os
 import config as cfg
 
 
-df = pd.read_csv('CLIPblind_pairs_kin_merge_final.csv')
+df = pd.read_csv('CLIPblind_pairs_ssv2_final.csv')
 
 print(df.head())
-vid_path = 'V-MMVP/kin400_pairs'
+vid_path = 'V-MMVP_ft/ssv2_pairs'
 os.makedirs(vid_path, exist_ok=True)
 
 for i, row in df.iterrows():
-    pair_path = f'{vid_path}/kin{i:03d}'
+    pair_path = f'{vid_path}/ss{i:03d}'
     os.makedirs(pair_path, exist_ok=True)
-    v1 = row['video1']
-    v2 = row['video2']
+    v1 = row['video1'].split(" ")[0]
+    v2 = row['video2'].split(" ")[0]
     # shutil.copy(f'{cfg.ucf101_path}/Videos/{v1.split("_")[1].replace("HandStand", "Handstand")}/{v1}', f'{pair_path}/{v1}')
     # shutil.copy(f'{cfg.ucf101_path}/Videos/{v2.split("_")[1].replace("HandStand", "Handstand")}/{v2}', f'{pair_path}/{v2}')
-    shutil.copy(f'{cfg.kinetics_path}/test/{v1}', f'{pair_path}/{v1}')
-    shutil.copy(f'{cfg.kinetics_path}/test/{v2}', f'{pair_path}/{v2}')
+    # shutil.copy(f'{cfg.kinetics_path}/test/{v1}', f'{pair_path}/{v1}')
+    # shutil.copy(f'{cfg.kinetics_path}/test/{v2}', f'{pair_path}/{v2}')
     # try:
     #     shutil.copy(f'{cfg.hmdb_vid_path}/{v1.split("_")[-7]}/{v1}', f'{pair_path}/{v1}')
     # except:
@@ -29,5 +29,7 @@ for i, row in df.iterrows():
     #     shutil.copy(f'{cfg.hmdb_vid_path}/{v2.split("_")[-7]}/{v2}', f'{pair_path}/{v2}')
     # except:
     #     shutil.copy(f'{cfg.hmdb_vid_path}/{v2.split("_")[-8]}_{v2.split("_")[-7]}/{v2}', f'{pair_path}/{v2}')
+    shutil.copy(f'{cfg.ssv2_videos}/{v1}', f'{pair_path}/{v1}')
+    shutil.copy(f'{cfg.ssv2_videos}/{v2}', f'{pair_path}/{v2}')
 
 print('Done!')
